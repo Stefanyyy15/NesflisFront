b6c431d9a6dc8b0f2968c3abf65e72f11f485b16
Stefanyyy15 <yuli.estefani.s15@gmail.com>
2025-01-23 18:40:22 -0500

    feat: :sparkles: Hicimos conexion con la API

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
+var url1 = "http://localhost:8080/api/administrador";
 
-let accordian = document.getElementsByClassName("FAQ__title");
 
-for (let i = 0; i < accordian.length; i++) {
-  accordian[i].addEventListener("click", function () {
-    if (this.childNodes[1].classList.contains("fa-plus")) {
-      this.childNodes[1].classList.remove("fa-plus");
-      this.childNodes[1].classList.add("fa-times");
-    } else {
-      this.childNodes[1].classList.remove("fa-times");
-      this.childNodes[1].classList.add("fa-plus");
-    }
-
-    let content = this.nextElementSibling;
-    if (content.style.maxHeight) {
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
-  });
+async function peticion(url) {
+  const respuesta = await fetch(url);
+  if (respuesta.ok) {
+    const info = await respuesta.json();
+    return info;
+  } else {
+    console.log("");
+    return [];
+  }
 }
+async function verPersonajes(url) {
+  const categorias = await peticion(url);
+  console.log(categorias);
+}
+
+verPersonajes(url1);
